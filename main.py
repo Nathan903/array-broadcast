cc5da548182b086ad9f67de17e4dda61ff9bb0e1
Nathan <30803215+Nathan903@users.noreply.github.com>
2023-11-17 02:37:50 -0500

    Update main.py

--- main.py
+++ main.py
@@ -3,73 +3,130 @@
 # for testing
 import numpy as np
 
-# will be converted to cpp vectors  
-arr1d = [1,2,3] # (3)
-arr2d = [
-    [10,10,10],
-    [20,20,20],
-    [30,30,30]
-]
-
-arr2d_2 = [[10], [20], [30]]  #(3,1)
+#use N-D python arrays for prototyping. (cpp vectors for production)
 
 def shape(a):
     """
-    gives shape of array
+    gives shape of array. This should be converted to a privated variable to reduce calculation
     """
     if type(a)!=list:
         return tuple()
     return (len(a),) + shape(a[0])
 
+def rank(a):
+    return len(shape(a))
 
-def add_with_boardcasting(a,b,ashape,bshape,result,resultshape):
-    pass
-    #determine shape of a, b and compare them
+def add_with_boardcasting(a,b):
+    """
+    add with broadcasting, like numpy
+    return None if broadcasting is not allowed
+    """
+
+    # base case which is when both items are numbers
+    if type(a)!=list and type(b)!=list:
+        return a+b
+
+    # ensure b is the one with less rank to be padded
+    if rank(b)>rank(a):
+        a,b=b,a
+
+    dimension_diff = rank(a)-rank(b)
+    padding =(1,)* (dimension_diff )
+    a_shape = shape(a)
+    b_shape = padding+ shape(b)
+    for i,j in zip(a_shape,b_shape):
+        if (i!=1 and i!=j and j!=1):
+            #CANNOT BROADCAST
+            return None
+
+    # wrap b into the same shape as b_shape
+    for i in range(dimension_diff):
+        b=[b]
+
+    result_shape = tuple([ max(i,j) for i,j in zip(a_shape,b_shape) ])
+
+
+    n=result_shape[0]
+
+    result = [0]*n # make the result as a 1*n vector
+
+    # 0 if this dim has single elemenent, otherwise 1
+    b_is_const = b_shape[0]==n
+    a_is_const = a_shape[0]==n 
+
+    for i in range(n): 
+        result[i] = add_with_boardcasting(a[i*a_is_const],b[i*b_is_const])
     
-    #lower rank array have shape padded with 1 in front
-    
-    # obtain the final shape by taking max() of each dim
-    # intial this 1d vector/array
-    # calculate index in flatten array 
-    #(3,1)
-    #(1,3)
-    #(3,3)
-    # if len(resultshape)==1:
-    #     #basecase
-    #     for i in range(resultshape[0]):
-    #         result[startindex+i]=a[...]+b[...]
-            
-    # else:
-    #     for i in range(resultshape[-2]): 
-    #         startindex = # 
-    #         add_with_boardcasting(a,b,ashape[:-1],bshape[:-1],result,resultshape[:-1],startindex)
-
-    # pass
+    return result
 
 
-#print(np.array(arr2d)+np.array(arr1d))
+print(add_with_boardcasting(
+    [1,2,3],
+    [[10],[20],[30]]
+))
 
-print(np.array(arr2d_2)+np.array(arr1d))
 
+def unit_test():
+    tests = {
+        'arr3' : [1,2,3],
+        'arr3x3' : [
+            [10,10,10],
+            [20,20,20],
+            [30,30,30]
+        ],
+        'arr3x2' : [
+            [10,170,102],
+            [26,208,230],
+        ],
+        'arr3x1' : [
+            [17], 
+            [25], 
+            [33]
+        ],
+        'arr1x1':[
+            [11], 
+        ],
+        'arr1':[3],
+        'arr':5,
+        'arr5x4':np.random.randint(1, 100, size=(5,4)).tolist(),
+        'arr4':np.random.randint(1, 100, size=(4,)).tolist(),
+        'arr15x3x5':np.random.randint(1, 100, size=(15,3,5)).tolist(),
+        'arr15x1x5':np.random.randint(1, 100, size=(15,1,5)).tolist(),
+        'arr3x1':np.random.randint(1, 100, size=(3,1)).tolist(),
+        'arr3x5':np.random.randint(1, 100, size=(3,5)).tolist(),
+    }
 
-"""
-A      (2d array):  5 x 4
-B      (1d array):      1
-Result (2d array):  5 x 4
+    def unit_test_shape(array_dict):
+        for name, arr in array_dict.items():
+            if shape(arr)!=np.array(arr).shape:
+                print("ERROR in shape:", name,arr)
+    unit_test_shape(tests)
 
-A      (2d array):  5 x 4
-B      (1d array):      4
-Result (2d array):  5 x 4
+    def unit_test_add(array_dict):
+        for name, arr in array_dict.items():
+            for name2, arr2 in tests.items():
+                result =np.array(add_with_boardcasting(arr,arr2))
+                try:
+                    reference = np.array(arr)+np.array(arr2)
+                except:
+                    reference = None
+                if not np.array_equal(reference,result):
+                    print(name,name2,reference)
+        for i in range(1000):
+            num_dimensions = np.random.randint(1, 6)
+            shape1 = tuple(np.random.randint(1, 7, size=num_dimensions))
+            arr = np.random.randint(1, 100, size=shape1).tolist()
 
-A      (3d array):  15 x 3 x 5
-B      (3d array):  15 x 1 x 5
-Result (3d array):  15 x 3 x 5
+            num_dimensions = np.random.randint(1, 6)
+            shape2 = tuple(np.random.randint(1, 7, size=num_dimensions))
+            arr2 = np.random.randint(1, 100, size=shape2).tolist()
+            result =np.array(add_with_boardcasting(arr,arr2))
+            try:
+                reference = np.array(arr)+np.array(arr2)
+            except:
+                reference = None
+            if not np.array_equal(reference,result):
+                print(name,name2,reference)
+    unit_test_add(tests)
 
-A      (3d array):  15 x 3 x 5
-B      (2d array):       3 x 5
-Result (3d array):  15 x 3 x 5
-
-A      (3d array):  15 x 3 x 5
-B      (2d array):       3 x 1
-Result (3d array):  15 x 3 x 5
-"""
+unit_test()
